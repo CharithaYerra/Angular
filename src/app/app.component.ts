038ae50913029c2efeebc1d1c6a43302ea782b10
grumpchick <divyacharitha20@gmail.com>
2025-08-07 17:48:23 +0530

    angular basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-reader',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularBasics';
+  classdate='05082025';
+  getStudentRoll(){
+    return 23339;
+  }
+
+  propertyBind='Binded';
+  inputType='checkbox';
+  bool=true;
+  btnClick(){
+  console.log("event binding");
+    
+  }
+  btnMouse(){
+    console.log("you are mouse in on button");
+  }
 }
---
+++ src/app/test-comp/test.component.ts
@@ -0,0 +1,11 @@
+//We have to take component features to file for angular
+import { Component}  from "@angular/core";
+@Component({
+    selector:'app-test-comp',
+    templateUrl:'./test.component.html',
+    styleUrls:['./test.component.css']
+})
+export class TestComponent{
+
+
+}
