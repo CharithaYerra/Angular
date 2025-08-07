7f61f03ad89dcbff06f88e9c1c840ae54792e0ca
grumpchick <divyacharitha20@gmail.com>
2025-08-07 18:48:07 +0530

    angular basis-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,5 +22,18 @@ export class AppComponent {
   btnMouse(){
     console.log("you are mouse in on button");
   }
-  
+  captureData(eventDetails:any){
+    // console.log("data captured")
+    console.log(eventDetails);
+    
+
+  }
+  capture(eventDetails:KeyboardEvent,statusIs:string){
+    // console.log("data captured")
+    console.log(eventDetails);
+    let elementData=eventDetails.target as HTMLInputElement;
+    console.log(elementData);
+
+
+  }
 }
